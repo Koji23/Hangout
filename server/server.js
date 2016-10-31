c9504c7aeb8779d4f60d479062d9e3772728d671
Jordan Chong <jordanchong23@gmail.com>
2016-10-30 17:12:54 -0700

    Create localhost ssl certificates and update https server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 const express = require('express'); // replace express with express.io
 const app = express();
 const fs = require('fs');
-var https = require('https');
+// const http = require('http');
+const https = require('https');
 const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.crt')
-}
-const server = https.createServer(options, app);
-const io = require('socket.io')(server);
-console.log(server);
+  key: fs.readFileSync('localhost.key'),
+  cert: fs.readFileSync('localhost.crt')
+};
+const httpsServer = https.createServer(options, app);
+const io = require('socket.io')(httpsServer);
 const middleware = require('./config/middleware');
 const routes = require('./config/routes');
-// var db = require('./config/database.js');
+
 const port = process.env.PORT || 3000;
 
 middleware(app, express);
@@ -20,8 +20,8 @@ routes(app, express, io);
 // io.sockets.in('chat').emit('someEvent2', 'what is going on, party people?');
 
 
-server.listen(port, function(err) {
+httpsServer.listen(port, function(err) {
   err ? console.log('Server error', err) : console.log('Server running on port', port);
 });
 
-module.exports = app;
+// module.exports = app;
